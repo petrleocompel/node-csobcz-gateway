a667577a8d5ddb898aa13726606e49dded1d900f
Lukáš Chalupa <luchalupa@gmail.com>
2023-03-13 13:09:49 +0100

    change default indentation

--- src/types/Logger.ts
+++ src/types/Logger.ts
@@ -1,6 +1,6 @@
 export type Logger = {
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  log: (message?: any, ...optionalParams: any[]) => void;
-  error: (message?: any, ...optionalParams: any[]) => void;
-  warn: (message?: any, ...optionalParams: any[]) => void;
+	/* eslint-disable @typescript-eslint/no-explicit-any */
+	log: (message?: any, ...optionalParams: any[]) => void;
+	error: (message?: any, ...optionalParams: any[]) => void;
+	warn: (message?: any, ...optionalParams: any[]) => void;
 };
